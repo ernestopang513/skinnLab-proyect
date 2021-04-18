3ebf9c9a378c3bdfbc2d187ac291b875c0f70f27
Ernesto Pang <ernestopang513@hotmail.com>
2021-04-17 23:44:29 -0500

    Pruebas de facebook post

--- src/components/aboutUs/AboutUs.js
+++ src/components/aboutUs/AboutUs.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Wrapper } from '../../stylish__src/stylish__src'
 
 export const AboutUs = () => {
     return (
@@ -6,6 +7,12 @@ export const AboutUs = () => {
             <h1>
                 About Us
             </h1>
+            <Wrapper>
+
+                <div id="fb-root "></div>
+                <div className="fb-post w-background" data-href="https://www.facebook.com/skinlabtatuajesyperforaciones/photos/a.145934059449251/755187435190574/" data-width="500" data-show-text="true"><blockquote cite="https://www.facebook.com/skinlabtatuajesyperforaciones/photos/a.145934059449251/755187435190574/?type=3" className="fb-xfbml-parse-ignore"><p>Microdermal realizada en SKIN LAB por &#064;corcholata666
+                Citas y cotizaciones disponibles por Messenger o WhatsApp...</p>Publicado por <a href="https://www.facebook.com/skinlabtatuajesyperforaciones/">SKIN LAB</a> en&nbsp;<a href="https://www.facebook.com/skinlabtatuajesyperforaciones/photos/a.145934059449251/755187435190574/?type=3">Viernes, 16 de abril de 2021</a></blockquote></div>
+            </Wrapper>
         </div>
     )
 }
