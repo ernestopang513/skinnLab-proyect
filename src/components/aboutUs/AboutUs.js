6043db707eb4d61b815cf120a0e87c0acffb1cdb
Ernesto Pang <ernestopang513@hotmail.com>
2021-04-19 02:04:39 -0500

    pruebas con iframe

--- src/components/aboutUs/AboutUs.js
+++ src/components/aboutUs/AboutUs.js
@@ -14,6 +14,9 @@ export const AboutUs = () => {
                 <div id="fb-root "></div>
                 <div className="fb-post w-background" data-href="https://www.facebook.com/skinlabtatuajesyperforaciones/photos/a.145934059449251/755187435190574/" data-width="350" data-show-text="true"><blockquote cite="https://www.facebook.com/skinlabtatuajesyperforaciones/photos/a.145934059449251/755187435190574/?type=3" className="fb-xfbml-parse-ignore"><p>Microdermal realizada en SKIN LAB por &#064;corcholata666
                 Citas y cotizaciones disponibles por Messenger o WhatsApp...</p>Publicado por <a href="https://www.facebook.com/skinlabtatuajesyperforaciones/">SKIN LAB</a> en&nbsp;<a href="https://www.facebook.com/skinlabtatuajesyperforaciones/photos/a.145934059449251/755187435190574/?type=3">Viernes, 16 de abril de 2021</a></blockquote></div>
+
+
+                <iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Fskinlabtatuajesyperforaciones%2Fvideos%2F744816142884556%2F&show_text=false&width=560" width={350} height={314} style={{border: 'none', overflow: 'hidden'}} scrolling="no" frameBorder={0} allowFullScreen={false} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" />
             </Wrapper>
         </div>
     )
